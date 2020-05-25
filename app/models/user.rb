d692695a6253d6931943cb746a5b5d6f6dafc4b5
makiharu <kitakazetoshirokuma@gmail.com>
2020-05-25 01:22:27 +0000

    [Finish]

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :confirmable
 
   has_many :books,dependent: :destroy
-  validates :introduction, presence: true, length: {maximum: 50}
+  validates :introduction, length: {maximum: 50}
   validates :name, presence: true
 
   attachment :profile_image
